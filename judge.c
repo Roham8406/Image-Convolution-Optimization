621a6203eb4dc742164d3fab2139ab23af404cb9
Roham <Roham8406@gmail.com>
2026-02-17 22:44:09 +0330

    minor change

--- judge.c
+++ judge.c
@@ -48,16 +48,16 @@ void checkImage(char *image, int isVicotrian) {
     
 
     loadMat("Assets/TowerCheck/Vert.txt", &data);
-    sum2 -= clock();
+    sum1 -= clock();
     convolveOptimized(data);
     long long c5 = threshold(data.out, 127, ((long long) (data.w)) * data.h * data.ch);
-    sum2 += clock();
+    sum1 += clock();
 
     loadMat("Assets/TowerCheck/Edge.txt", &data);
-    sum2 -= clock();
+    sum1 -= clock();
     convolveOptimized(data);
     long long c6 = threshold(data.out, 50, ((long long) (data.w)) * data.h * data.ch);
-    sum2 += clock();
+    sum1 += clock();
 
     double score = calc(c1,c2,c3,c4,c5,c6, ((long long) (data.w)) * data.h * data.ch);
 
@@ -66,11 +66,11 @@ void checkImage(char *image, int isVicotrian) {
 
     tidyup(&data);
     printf("*** %s ***\n", image);
-    printf("\t %lf \t %lf \t %lf \t %lf", (double)c1/sum, (double)c2/sum, (double)c3/sum, (double)c4/sum);
-    printf("\t %lf \t %lf\n", (double)c5/sum, (double)c6/sum);
-    printf(isVicotrian ? "Victorian\n" : "Iraninan\n");
-    printf(score < 0.4 ? "Likely Victorian": "Likely Iranian");
-    printf("\n%lf", score);
+    printf("Progress took: \t\t %2.6fms\n", (double) sum1 / CLOCKS_PER_SEC * 1000);
+    printf("Victorian sharp edges count: \t %lld \t %lld\n", c5, c6);
+    printf("Iraninan sharp edges count: \t %lld \t %lld \t %lld \t %lld\n", c1, c2, c3, c4);
+    printf("Score: %f\n", score);
+    printf(score < 0.25 ? "The Architecture is Victorian." : "The Architecture is Iraninan.");
     printf("\n****************************************\n\n\n");
 }
 
