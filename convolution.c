e84f2a7cb644f46dcca385748b0cb260350e9937
Roham <Roham8406@gmail.com>
2026-02-25 21:03:27 +0330

    feat: Added comments for convolution.c

--- convolution.c
+++ convolution.c
@@ -3,11 +3,10 @@
 void convolve(FilesDTO input) {
     int sum = 0;
     for (int i = input.deg*input.deg-1; i >= 0 ; i--) {
-        sum += input.mat[i];
+        sum += input.mat[i];                                                            //محاسبهٔ جمع درایه‌های ماتریس
     }
     sum = sum > 0 ? sum : 1;
     int centre = input.deg/2;
-    // int qwert = input.ch - 1 ? input.ch - 1 : 1;
     for (int i = centre; i < input.w - centre; i++) {
         for (int j = centre; j < input.h - centre; j++) {
             for (int k = 0; k < input.ch; k++) {
@@ -15,17 +14,18 @@ void convolve(FilesDTO input) {
                 for (int l = -centre; l <= centre; l++) {
                     for (int m = -centre; m <= centre; m++) {
                         int i1 = i + l, j1 = j + m;
-                        i1 = i1 >= input.w ? i1-input.w : i1 < 0 ? i1 + input.w : i1;
-                        j1 = j1 >= input.h ? j1-input.h : j1 < 0 ? j1 + input.h : j1;
-                        cSum += input.in[(j1 * input.w + i1) * input.ch + k] * input.mat[(m + centre) * input.deg  + l + centre];
+                        cSum += input.in[(j1 * input.w + i1) * input.ch + k] * 
+                                input.mat[(m + centre) * input.deg  + l + centre];      //محاسبهٔ ضرب درایهٔ ماتریس
                     }
                 }
 
-                cSum /= sum;
+                cSum /= sum;                                                            //هنجار کردن درایهٔ ماتریس
                 cSum = cSum > 255 ? 255 : cSum < 0 ? 0 : cSum;
-                input.out[(j*input.w + i)*input.ch+ k] = (unsigned char)cSum;
+                input.out[(j*input.w + i)*input.ch+ k] = (unsigned char)cSum;           //ذخیرهٔ درایهٔ ماتریس
             }
-        if (input.ch != 1) input.out[(j*input.w + i)*input.ch+ input.ch - 1] = input.in[(j*input.w + i)*input.ch+ input.ch - 1];
+        if (input.ch != 1 && sum<=0) 
+            input.out[(j*input.w + i)*input.ch+ input.ch - 1] = 
+                input.in[(j*input.w + i)*input.ch+ input.ch - 1];                       //جلوگیری از بی‌رنگی‌ توسط 0 شدن کانال آلفا
         }
     }
 }
